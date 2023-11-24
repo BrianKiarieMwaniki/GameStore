3481d6e6ea067f1bd8e8c26d238bb01ce84545a6
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2023-11-24 09:58:30 +0300

    setup mssql image for docker

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -2,6 +2,9 @@ using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+var gameStoreConnectionString = configuration.GetConnectionString("GameStoreContext");
 
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>(); 
 
