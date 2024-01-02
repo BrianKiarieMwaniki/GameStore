ff94f121f378b4377c9b7efa808fad8723aed49d
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-01-02 19:58:02 +0300

    updated the way I read roles form the auth0 token

--- GameStore.Api/Authorization/ScopeTransformation.cs
+++ GameStore.Api/Authorization/ScopeTransformation.cs
@@ -5,26 +5,41 @@ namespace GameStore.Api.Authorization;
 
 public class ScopeTransformation : IClaimsTransformation
 {
-    private const string scopeClaimName = "Scope";
+    private const string scopeClaimName = "scope";
     public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
         var scopeClaim = principal.FindFirst(scopeClaimName);
 
-        if(scopeClaim is null) return Task.FromResult(principal);
+        if (scopeClaim is null) return Task.FromResult(principal);
 
         var scopes = scopeClaim.Value.Split(' ');
 
+
         var originalIdentity = principal.Identity as ClaimsIdentity;
+
 
         var identity = new ClaimsIdentity(originalIdentity);
 
         var originalScopeClaim = identity.Claims.FirstOrDefault(claim => claim.Type == scopeClaimName);
-        if(originalScopeClaim is not null)
+        if (originalScopeClaim is not null)
         {
             identity.RemoveClaim(originalScopeClaim);
         }
 
+        var roleClaims = originalIdentity?.Claims
+                    .Where(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/roles")
+                    .ToList();
+
         identity.AddClaims(scopes.Select(scope => new Claim(scopeClaimName, scope)));
+
+        if (roleClaims != null && roleClaims.Any())
+        {
+            foreach (var claim in roleClaims)
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, claim.Value));
+            }
+        }
+
 
         return Task.FromResult(new ClaimsPrincipal(identity));
     }
