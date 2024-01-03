5e4acbe6843c41ca992e3923ad7a0f501f504eaa
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-01-03 13:31:29 +0300

    created and registered an imageuploader class

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,11 @@
 using Asp.Versioning;
+using Azure.Storage.Blobs;
 using GameStore.Api.Authorization;
 using GameStore.Api.Cors;
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.ErrorHandling;
+using GameStore.Api.ImageUpload;
 using GameStore.Api.Middleware;
 using GameStore.Api.OpenAPI;
 using Microsoft.Extensions.Options;
@@ -32,6 +34,15 @@ builder.Services.AddGameStoreCors(configuration);
 builder.Services.AddSwaggerGen()
                 .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>()
                 .AddEndpointsApiExplorer();
+
+builder.Services.AddSingleton<IImageUploader>(
+    new ImageUploader(
+        new BlobContainerClient(
+            builder.Configuration.GetConnectionString("AzureStorage"), 
+            "images"
+        )
+    )
+);
 
 var app = builder.Build();
 
